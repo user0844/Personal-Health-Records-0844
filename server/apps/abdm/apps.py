bcf3d8ed3b6c8bec49c9f09a037243ba4a6ef931
user0844 <cave-tallies.8f@icloud.com>
2025-07-09 16:56:30 +0530

    Update apps.py

--- server/apps/abdm/apps.py
+++ server/apps/abdm/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class AbdmConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.abdm'
+    name = 'sever.apps.abdm'    # 'apps.abdm'
